0adf9e17e70d19548c9c72f19c80c55563aae2d7
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-04-25 10:56:24 +0200

    refactor: signal resource

--- libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,30 +1,25 @@
 import { NgIf } from '@angular/common';
-import { Component, effect, inject, input, signal } from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Component, effect, inject, input, numberAttribute } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { validatePassengerStatus } from '../../util-validation';
-import { initialPassenger } from '../../logic-passenger';
+import { RouterLink } from '@angular/router';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
-import { switchMap } from 'rxjs';
+import { validatePassengerStatus } from '../../util-validation';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
   private passengerService = inject(PassengerService);
 
-  id = input(0);
-  passenger = toSignal(
-    toObservable(this.id).pipe(
-      switchMap(id => this.passengerService.findById(id))
-    ), { initialValue: initialPassenger }
-  );
+  id = input(0, { transform: numberAttribute });
+  passengerResource = this.passengerService.findByIdAsResource(this.id);
 
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
@@ -37,7 +32,13 @@ export class PassengerEditComponent {
   });
 
   constructor() {
-    effect(() => this.editForm.patchValue(this.passenger()));
+    effect(() => {
+      const passenger = this.passengerResource.value();
+
+      if (passenger) {
+        this.editForm.patchValue(passenger);
+      }
+    });
   }
 
   protected save(): void {
